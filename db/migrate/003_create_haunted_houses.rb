6e37aebf0269a14e12cbbb2e28b7dd7fdf7d6a68
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-05-12 05:32:23 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,8 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2] 
   def change 
     create_table :haunted_houses do |h| 
-      h.string :name 
+      h.string :name
+      h.string :location
       
     end 
   end 
