a57bcbb83da52567776049de6f0d5ffddac15445
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-05-12 05:18:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,6 @@
 # Create your costume_stores migration here
 
-class CostumeStore < ActiveRecord::Migration 
+class CostumeStore < ActiveRecord::Migration[4.2] 
   def change 
     def create_table 
     end 
