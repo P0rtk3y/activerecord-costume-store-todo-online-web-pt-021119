0781abb2b52093cfab4521c96b0317804aae6cd8
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-05-12 05:38:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,7 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       h.boolean :family_friendly
       h.datetime :opening_date
       h.datetime :closing_date
-      h.string :get_description
+      h.string :description
     end 
   end 
 end 
